6106c037e78e198dfaf7dbcf96f07c17c1fe0e28
2547techno <technowizz2547@gmail.com>
2022-11-12 13:30:40 -0500

    refactor

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 rm -rf out
 mkdir out
-cp manifest.json out/manifest.json
+cp src/manifest.json out/manifest.json
 cp -r src/ out/src
-cp -r icons/ out/icons
+cp -r src/icons/ out/icons
 cd out
 zip out.zip . -r
